0b277fd5f247ee91fec8dc451023b6e5b849c552
cloudcompassconsulting <131810203+cloudcompassconsulting@users.noreply.github.com>
2023-05-04 22:34:09 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Prompt the user to enter the necessary values
+echo "Enter the following values:"
+read -p "Database name: " DB_NAME
+read -p "Database user: " DB_USER
+read -p "Database password: " DB_PASSWORD
+read -p "Site URL: " SITE_URL
+read -p "Site title: " SITE_TITLE
+read -p "Admin username: " ADMIN_USER
+read -p "Admin password: " ADMIN_PASSWORD
+read -p "Admin email: " ADMIN_EMAIL
+
 # Update the system packages
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -14,8 +25,11 @@ sudo mv wp-cli.phar /usr/local/bin/wp
 
 # Download and install WordPress
 sudo wp core download --path=/var/www/html --allow-root
-sudo wp core config --path=/var/www/html --dbname=DB_NAME --dbuser=DB_USER --dbpass=DB_PASSWORD --dbhost=localhost --dbprefix=wp_ --allow-root
-sudo wp core install --path=/var/www/html --url=http://PUBLIC_IP_ADDRESS_OR_DOMAIN_NAME --title="My WordPress Site" --admin_user=admin --admin_password=PASSWORD --admin_email=EMAIL --allow-root
+sudo wp config create --path=/var/www/html --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=localhost --dbprefix=wp_ --extra-php <<PHP
+define( 'WP_HOME', '$SITE_URL' );
+define( 'WP_SITEURL', '$SITE_URL' );
+PHP
+sudo wp core install --path=/var/www/html --url="$SITE_URL" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root
 
 # Set file permissions
 sudo chown -R www-data:www-data /var/www/html/
